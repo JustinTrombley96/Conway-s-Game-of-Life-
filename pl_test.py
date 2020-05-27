666962b79ea82e62f5d7c83a8e7cb2beaba0f838
Justin Trombley <jtrombley96@gmail.com>
2020-05-27 17:33:26 -0400

    20x20 Creator

--- pl_test.py
+++ pl_test.py
@@ -1,14 +1,19 @@
 from PIL import Image
 import os
 
-size_300 = (300, 300)
+size_20 = (20, 20)
 
 for f in os.listdir('.'):
+    if f.endswith('.png'):
+        i = Image.open(f)
+        fn, fext = os.path.splitext(f)
+        i.thumbnail(size_20)
+        i.save('20/{}_20{}'.format(fn, fext))
     if f.endswith('.jpg'):
         i = Image.open(f)
         fn, fext = os.path.splitext(f)
-        i.thumbnail(size_300)
-        i.save('300/{}_300{}'.format(fn, fext))
-alive_cell = Image.open('blue_block.png')
-dead_cell = Image.open('white_block.jpg')
+        i.thumbnail(size_20)
+        i.save('20/{}_20{}'.format(fn, fext))
+alive_cell = Image.open('alive_block.png')
+dead_cell = Image.open('dead_block.jpg')
 alive_cell.show()
