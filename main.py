193b12893836e1df04590909c5874e12fd4b35b8
Justin Trombley <jtrombley96@gmail.com>
2020-05-28 19:23:09 -0400

    Map changes

---
+++ main.py
@@ -0,0 +1,4 @@
+from table import Table
+
+table = Table()
+table.print_grid()
--- table.py
+++ table.py
@@ -1,8 +1,8 @@
 class Table:
     def __init__(self):
-        self.grid = [['1', '2', '3'],
-                     ['4', '5', '6'],
-                     ['7', '8', '9']]
+        self.grid = [['TL', 'T', 'TR'],
+                     ['L', 'M', 'R'],
+                     ['BL', 'B', 'BR']]
     
     def print_grid(self):
         for row in self.grid:
