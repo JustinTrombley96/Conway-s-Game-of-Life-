7f3aa4147208eea2ea1cbd7a9825bab641d2c74e
Justin Trombley <jtrombley96@gmail.com>
2020-05-27 20:34:55 -0400

    Class Cell

--- cell.py
+++ cell.py
@@ -4,3 +4,16 @@
 # Set Cells to Alive
 # Set Cells to Dead
 # Check if the Cell is Alive 
+
+class Cell:
+    def __init__(self):
+        self.status = 'Dead'
+    def set_cells_alive(self):
+        self.status = 'Alive'
+    def set_cells_dead(self):
+        set.status = 'Dead'
+    def cell_check(self):
+        if self.status == 'Alive':
+            return True
+        return False
+        
